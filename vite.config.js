e8c7cc79c63ba9239a2980c8c07da9aa90b5b194
saam-kim <kjs59741@gmail.com>
2026-04-27 21:49:01 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/constitution-game-react/
+  base: '/constitution-game-react/'
   plugins: [react()],
 })
